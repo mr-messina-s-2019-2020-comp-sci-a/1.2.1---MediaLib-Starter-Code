14210ed890028a280c06dca8743ad879a0280da6
gharper11 <55795447+gharper11@users.noreply.github.com>
2019-10-09 08:01:51 -0500

    MediaLib
    Media lib code

---
+++ Cost.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Cost here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Cost
+{
+    /*private double cost;
+    private int numSongs;
+    
+        public Cost()
+    
+    {
+        cost = 0.0;
+    }
+    public double getCost() {
+        return cost;
+    }
+    public void setCost(double c) {
+        cost = 0.0;
+    }*/
+}
---
+++ Song.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Song here.
+ *
+ * @author (Garrett Harper)
+ * @version (8/19/2019)
+ */
+public class Song 
+{
+
+    private String title;
+    private int rating;
+    private double cost;
+    
+    public Song()
+{
+    rating = 0;
+    
+    title  = "";
+    
+    cost = 0.0;
+}
+public String getTitle() {
+        return title;
+    }
+public void setTitle(String t) {
+        title = t;
+    }
+    public double getCost() {
+ return cost;
+}
+public void setCost(double c) {
+        cost = c;
+}
+}
